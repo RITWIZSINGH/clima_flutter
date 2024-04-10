8ba3e88a492f0786b4004f5f8f851babf4733c06
Ritwiz <madangopalsingh2803@gmail.com>
2024-04-10 15:15:43 +0530

    Initial commit + added resources

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,15 @@
-//Initial Commit
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+import 'package:clima_flutter/screens/loading_screen.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
+    );
+  }
+}
---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors, library_private_types_in_public_api, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //Get the current location
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
